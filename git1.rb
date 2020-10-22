25ffe943fc2c40ea4abb9121689fec8fe5b054c0
Nishikori Hirari <j1726@matsue-ct.jp>
2020-10-22 16:55:22 +0900

    first commit

---
+++ git1.rb
@@ -0,0 +1,11 @@
+#!/home/hirari/git-tutorial
+#encoding: UTF-8
+
+for i in 1..100 do
+ if (i % 3 == 0 && i % 5 == 0) then
+	print("FizzBuzz\n")
+ elsif (i % 3 == 0) then
+ elsif (i % 5 == 0) then
+	print("Buzz\n")
+ end
+end
